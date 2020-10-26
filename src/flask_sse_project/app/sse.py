a27a88bfee081552f6094a711a3ee6e658b67832
Luca Mülke <lucamuelke@gmail.com>
2020-10-26 19:36:13 +0100

    Catch all

--- src/flask_sse_project/app/sse.py
+++ src/flask_sse_project/app/sse.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
 
-@app.route('/')
-def index():
+@app.route('/', defaults = {'path' : ''})
+@app.route('/<path:path>')                  #Catch all
+def index(path):
     return render_template("index.html")
 
 @app.route('/hello')
