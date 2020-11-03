dca6534fdb922ad6f6806ae7675b2094e2cdd1f9
Andre Kuhlage <a.kuhlage@stud.phwt.de>
2020-11-03 15:14:29 +0100

    added bluetooth_controller.py

---
+++ src/blutooth_controller.py
@@ -0,0 +1,34 @@
+import evdev
+
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+device_path = None
+for device in devices:
+  if (device.name == "SmartRemote Consumer Control"):
+    print("Device found")
+    device_path = device.path
+
+if (not device_path):
+  print("No Device found")
+
+if(device_path):
+  device = evdev.InputDevice(device_path)
+  print(device)
+  for event in device.read_loop():
+    if (event.type == evdev.ecodes.EV_KEY):
+      """ print(evdev.categorize(event).scancode) # Ist Eventcode
+      print(evdev.categorize(event).keycode)  # Ist übersetzter Eventcode
+      print(event.type) """
+      scancode = evdev.categorize(event).scancode
+      if (evdev.categorize(event).keystate == 0):
+        if(scancode == 115):
+          print("up")
+        elif(scancode == 114):
+          print("down")
+        elif(scancode == 163):
+          print("right")
+        elif(scancode == 165):
+          print("left")
+        elif(scancode == 164):
+          print("ok")
+        else:
+          print("unknown")
