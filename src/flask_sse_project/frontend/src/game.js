6d4e40ebae89360a6bd4af9fdaa24c982862146f
niklas <niklas.bavendamm@stud.phwt.de>
2020-12-17 16:17:40 +0100

    team1left auf das Backend angepasst

--- src/flask_sse_project/frontend/src/game.js
+++ src/flask_sse_project/frontend/src/game.js
@@ -17,17 +17,11 @@ function Game(props) {
                 ? <Counter isRight="1" counter={props.data.counterTeam2} rounds={props.data.roundsTeam2} games={props.data.gamesTeam2}/>
                 : <Counter isRight="1" counter={props.data.counterTeam1} rounds={props.data.roundsTeam1} games={props.data.gamesTeam1}/>}
                 
-                {props.data.team1Left 
-                ? <Teamview colorHighSite={props.data.team1HighColor} colorDownSite={props.data.team1DownColor}
+                <Teamview colorHighSite={props.data.team1HighColor} colorDownSite={props.data.team1DownColor}
                 opacityHighSite={props.data.opacityHighSiteTeam1} opacityDownSite={props.data.opacityDownSiteTeam1}/>
-                : <Teamview colorHighSite={props.data.team2HighColor} colorDownSite={props.data.team2DownColor}
-                opacityHighSite={props.data.opacityHighSiteTeam2} opacityDownSite={props.data.opacityDownSiteTeam2}/>}
 
-                {props.data.team1Left 
-                ? <Teamview colorHighSite={props.data.team2HighColor} colorDownSite={props.data.team2DownColor}
+                <Teamview colorHighSite={props.data.team2HighColor} colorDownSite={props.data.team2DownColor}
                 opacityHighSite={props.data.opacityHighSiteTeam2} opacityDownSite={props.data.opacityDownSiteTeam2}/>
-                : <Teamview colorHighSite={props.data.team1HighColor} colorDownSite={props.data.team1DownColor}
-                opacityHighSite={props.data.opacityHighSiteTeam1} opacityDownSite={props.data.opacityDownSiteTeam1}/>}
 
             </div>
        )
